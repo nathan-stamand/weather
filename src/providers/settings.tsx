605e404b7e2bbc4cbbb96f0faf7e8f4dd2a5e7e6
Nathan St.Amand <nathansaint810@gmail.com>
2024-11-30 12:24:54 -0600

    Add settings provider

---
+++ src/providers/settings.tsx
@@ -0,0 +1,57 @@
+import { ReactNode, useContext, createContext, useState, Dispatch, SetStateAction } from "react";
+
+type Setting = {
+  value: boolean;
+  default: string;
+}
+
+const DEFAULT_SETTINGS: Record<string, Setting> = {
+  fahrenheit: {
+    value: true,
+    default: 'celcius'
+  },
+  miles: {
+    value: true,
+    default: 'kilometers'
+  },
+}
+
+type SettingsContext = {
+  settings: typeof DEFAULT_SETTINGS;
+  setSettings: Dispatch<SetStateAction<typeof DEFAULT_SETTINGS>>;
+}
+
+const Settings = createContext<SettingsContext>({
+  settings: DEFAULT_SETTINGS,
+  setSettings: () => { },
+});
+
+export const SettingsProvider = ({ children }: { children: ReactNode }) => {
+  const [settings, setSettings] = useState(DEFAULT_SETTINGS);
+
+  return (
+    <Settings.Provider value={{ settings, setSettings }}>
+      {children}
+    </Settings.Provider>
+  );
+}
+
+export const useSettings = () => {
+  const { settings, setSettings } = useContext(Settings);
+
+  const updateSetting = (setting: keyof typeof DEFAULT_SETTINGS | string, value: boolean) => {
+    if (!(setting in DEFAULT_SETTINGS)) return;
+    const settingObj = {
+      [setting]: {
+        value,
+        default: settings[setting]['default']
+      }
+    };
+    setSettings({ ...settings, ...settingObj });
+  }
+
+  return {
+    settings,
+    updateSetting,
+  }
+}
