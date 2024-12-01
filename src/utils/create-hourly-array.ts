f87f74b0c3f1848ef5639a19991fd05ea19b26ab
Nathan St.Amand <nathansaint810@gmail.com>
2024-12-01 08:31:30 -0600

    Generate smaller object array for chart

--- src/utils/create-hourly-array.ts
+++ src/utils/create-hourly-array.ts
@@ -5,9 +5,10 @@ export function createHourlyArray(startTime: string, hours: Hour[], fahrenheit: 
   const date = new Date(startTime);
   const currentHour = date.getHours();
   const twelveHours = hours.slice(currentHour, currentHour + 24);
-  twelveHours.forEach(hr => {
-    hr.formattedHour = getFormattedTime(hr.time);
-    hr.temperature = fahrenheit ? hr.temp_f : hr.temp_c;
+  return twelveHours.map(hr => {
+    return {
+      formattedHour: getFormattedTime(hr.time),
+      temperature: fahrenheit ? hr.temp_f : hr.temp_c,
+    }
   });
-  return twelveHours;
 }
