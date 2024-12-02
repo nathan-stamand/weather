6a7e4fbe48478d395f8bf6f2991c4f46ff5c6b12
Nathan St.Amand <nathansaint810@gmail.com>
2024-12-01 18:52:57 -0600

    Add responsiveness to module grid

--- src/features/forecast/components/modules/index.tsx
+++ src/features/forecast/components/modules/index.tsx
@@ -14,7 +14,7 @@ export const Modules = () => {
   return (
     <>
       <Title size='h2'>Current Data</Title>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <UVIndex />
         <WindStatus />
         <SunriseSunset />
